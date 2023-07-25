de6f794d68c932fd05f7bc3150631c5400dfc1fd
Cong Dinh <congdinh2012@hotmail.com>
2023-07-25 15:27:06 +0700

    Update read image with axios and buffer

--- server/src/helpers/util.ts
+++ server/src/helpers/util.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import Jimp from "jimp";
+import axios from "axios";
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -11,9 +12,15 @@ import Jimp from "jimp";
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const photoBuffer = await axios.get(inputURL, {
+        responseType: "arraybuffer",
+      })
+      
+      const photo = await Jimp.read(Buffer.from(photoBuffer?.data, "binary"));
+
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+        
       await photo
         .resize(1920, 1080) // resize
         .quality(100) // set JPEG quality
